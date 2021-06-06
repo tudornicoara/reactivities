d3524795071ad02a0b7815a1a09e5bbbc945ca21
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-06-06 17:12:54 +0300

    Set up auto migrate

--- Reactivities/Program.cs
+++ Reactivities/Program.cs
@@ -1,5 +1,10 @@
+using System;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Reactivities.Data;
 
 namespace Reactivities
 {
@@ -7,7 +12,25 @@ namespace Reactivities
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using var scope = host.Services.CreateScope();
+
+            var services = scope.ServiceProvider;
+
+            try
+            {
+                var context = services.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
+            catch (Exception e)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError(e, "An error occured during migration");
+                throw;
+            }
+            
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
