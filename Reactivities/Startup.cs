cee63a6c3875e5d8cae75210bb67f250af244a48
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-07-05 20:28:58 +0300

    Expanded CORS and disabled browser

--- Reactivities/Startup.cs
+++ Reactivities/Startup.cs
@@ -38,6 +38,8 @@ namespace Reactivities
                 opt.AddPolicy("CorsPolicy", policy =>
                 {
                     policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("https://localhost:5001");
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://192.168.1.37:3000");
                 });
             });
         }
