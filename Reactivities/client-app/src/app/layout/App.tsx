bf606982c32cdcb1ad3bd7fe80442ce3bd000e54
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-10-02 22:28:53 +0300

    Coloring and Activity Dashboard

--- Reactivities/client-app/src/app/layout/App.tsx
+++ Reactivities/client-app/src/app/layout/App.tsx
@@ -1,7 +1,9 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import {Header, List} from "semantic-ui-react";
+import {Container} from "semantic-ui-react";
 import {Activity} from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([]);
@@ -13,16 +15,12 @@ function App() {
   }, [])
   
   return (
-    <div>
-      <Header as='h2' icon='users' content='Reactivities' />
-        <List>
-          {activities.map((activity) => (
-              <List.Item key={activity.id}>
-                {activity.title}
-              </List.Item>
-          ))}
-        </List>
-    </div>
+    <>
+      <NavBar/>
+      <Container style={{marginTop: '7em'}}>
+        <ActivityDashboard activities={activities} />
+      </Container>
+    </>
   );
 }
 
