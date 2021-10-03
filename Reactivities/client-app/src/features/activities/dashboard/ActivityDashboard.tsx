53c7cbdc4b9fe85f6a0080f43ec145008b7579a1
INTEGRISOFT\tudor.nicoara <tudor.nicoara@integrisoft.ro>
2021-10-04 00:31:32 +0300

    Loading interceptor

--- Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ Reactivities/client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -15,11 +15,13 @@ interface Props {
     closeForm: () => void;
     createOrEdit: (activity: Activity) => void;
     deleteActivity: (id: string) => void;
+    submitting: boolean;
 }
 
 export default function  ActivityDashboard(
     {activities, selectedActivity, selectActivity, cancelSelectActivity,
-        editMode, openForm, closeForm, createOrEdit, deleteActivity}: Props) {
+        editMode, openForm, closeForm, createOrEdit,
+        deleteActivity, submitting}: Props) {
     return (
         <Grid>
             <Grid.Column width='10'>
@@ -27,6 +29,7 @@ export default function  ActivityDashboard(
                     activities={activities}
                     selectActivity={selectActivity}
                     deleteActivity={deleteActivity}
+                    submitting={submitting}
                 />
             </Grid.Column>
             <Grid.Column width='6'>
@@ -37,7 +40,12 @@ export default function  ActivityDashboard(
                     openForm={openForm}
                 />}
                 {editMode &&
-                <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit} />}
+                <ActivityForm
+                    closeForm={closeForm}
+                    activity={selectedActivity}
+                    createOrEdit={createOrEdit}
+                    submitting={submitting}
+                />}
             </Grid.Column>
         </Grid>
     )
