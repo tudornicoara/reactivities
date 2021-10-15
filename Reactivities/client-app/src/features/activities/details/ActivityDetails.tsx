26d0f53ec0f42c83373dbce67d1fb98e4ceb18aa
ENDAVA\tunicoara <Tudor.Nicoara@endava.com>
2021-10-15 16:40:00 +0300

    Added routing

--- Reactivities/client-app/src/features/activities/details/ActivityDetails.tsx
+++ Reactivities/client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {Button, Card, Image} from "semantic-ui-react";
 import {useStore} from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import {useParams} from "react-router-dom";
+import {observer} from "mobx-react-lite";
 
-export default function ActivityDetails() {
-    
+export default observer(function ActivityDetails() {
     const {activityStore} = useStore();
-    const {selectedActivity: activity, openForm, cancelSelectedActivity} = activityStore;
+    const {selectedActivity: activity, loadActivity, loadingInitial} = activityStore;
+    const {id} = useParams<{id: string}>();
     
-    if (!activity) return <LoadingComponent/>;
+    useEffect(() => {
+        if (id) {
+            loadActivity(id);
+        }
+    }, [id, loadActivity])
+    
+    if (loadingInitial || !activity) return <LoadingComponent/>;
     
     return (
         <Card fluid>
@@ -24,10 +32,10 @@ export default function ActivityDetails() {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths='2'>
-                    <Button onClick={() => openForm(activity.id)} basic color='blue' content='Edit' />
-                    <Button onClick={cancelSelectedActivity} basic color='grey' content='Cancel' />
+                    <Button basic color='blue' content='Edit' />
+                    <Button basic color='grey' content='Cancel' />
                 </Button.Group>
             </Card.Content>
         </Card>
     )
-}
+})
