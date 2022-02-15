8d3f703965faa59f633785564f4e839b1352d5fb
Tudor Nicoara <nicoaratudor@gmail.com>
2022-02-15 21:33:48 +0200

    Profile Bio editing

--- Reactivities/client-app/src/features/profiles/ProfileContent.tsx
+++ Reactivities/client-app/src/features/profiles/ProfileContent.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import {Tab} from "semantic-ui-react";
-import ProfilePhotots from "./ProfilePhotos";
+import ProfilePhotos from "./ProfilePhotos";
 import {Profile} from "../../app/models/profile";
 import {observer} from "mobx-react-lite";
+import ProfileAbout from "./ProfileAbout";
 
 interface Props {
     profile: Profile;
@@ -10,8 +11,8 @@ interface Props {
 
 export default observer(function ProfileContent({profile}: Props) {
     const panes = [
-        {menuItem: 'About', render: () => <Tab.Pane>About Content</Tab.Pane>},
-        {menuItem: 'Photos', render: () => <ProfilePhotots profile={profile} />},
+        {menuItem: 'About', render: () => <ProfileAbout />},
+        {menuItem: 'Photos', render: () => <ProfilePhotos profile={profile} />},
         {menuItem: 'Events', render: () => <Tab.Pane>Events Content</Tab.Pane>},
         {menuItem: 'Followers', render: () => <Tab.Pane>Followers Content</Tab.Pane>},
         {menuItem: 'Following', render: () => <Tab.Pane>Following Content</Tab.Pane>}
