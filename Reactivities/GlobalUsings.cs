d2d5805eaf21b1fe6aee529f0d371f2fa4171021
ENDAVA\tunicoara <Tudor.Nicoara@endava.com>
2022-02-21 17:38:57 +0200

    Update to .net 6 and update client packages

---
+++ Reactivities/GlobalUsings.cs
@@ -0,0 +1,16 @@
+﻿global using System;
+global using System.Threading.Tasks;
+global using FluentValidation.AspNetCore;
+global using Microsoft.AspNetCore.Authorization;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Mvc.Authorization;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.DependencyInjection;
+global using Microsoft.Extensions.Hosting;
+global using Microsoft.Extensions.Logging;
+global using Reactivities.Data;
+global using Reactivities.Entities;
+global using Reactivities.Extensions;
+global using Reactivities.Middleware;
+global using Reactivities.SignalR;
--- Reactivities/Program.cs
+++ Reactivities/Program.cs
@@ -1,48 +1,85 @@
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Reactivities.Data;
-using Reactivities.Entities;
+using Reactivities.Activities;
 
-namespace Reactivities
-{
-    public class Program
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to container
+builder.Services.AddControllersWithViews(opt =>
     {
-        public static async Task Main(string[] args)
-        {
-            var host = CreateHostBuilder(args).Build();
+        var policy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+        opt.Filters.Add(new AuthorizeFilter(policy));
+    })
+    .AddFluentValidation(config =>
+    {
+        config.RegisterValidatorsFromAssemblyContaining<Create>();
+    });
 
-            using var scope = host.Services.CreateScope();
+// In production, the React files will be served from this directory
+builder.Services.AddSpaStaticFiles(configuration =>
+{
+    configuration.RootPath = "ClientApp/build";
+});
 
-            var services = scope.ServiceProvider;
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityServices(builder.Configuration);
 
-            try
-            {
-                var context = services.GetRequiredService<DataContext>();
-                var userManager = services.GetRequiredService<UserManager<AppUser>>();
-                await context.Database.MigrateAsync();
-                await Seed.SeedData(context, userManager);
-            }
-            catch (Exception e)
-            {
-                var logger = services.GetRequiredService<ILogger<Program>>();
-                logger.LogError(e, "An error occured during migration");
-                throw;
-            }
+// Configure the HTTP request pipeline
+
+var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
             
-            await host.RunAsync();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Reactivities"));
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+//app.UseSpaStaticFiles();
+
+app.UseCors("CorsPolicy");
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+app.MapHub<ChatHub>("/chat");
+
+// app.UseSpa(spa =>
+// {
+//     spa.Options.SourcePath = "client-app";
+//             
+//     if (app.Environment.IsDevelopment())
+//     {
+//         //spa.UseReactDevelopmentServer(npmScript: "start");
+//         spa.UseProxyToSpaDevelopmentServer("http://localhost:3000");
+//     }
+// });
+
+using var scope = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+
+try
+{
+ var context = services.GetRequiredService<DataContext>();
+ var userManager = services.GetRequiredService<UserManager<AppUser>>();
+ await context.Database.MigrateAsync();
+ await Seed.SeedData(context, userManager);
+}
+catch (Exception e)
+{
+ var logger = services.GetRequiredService<ILogger<Program>>();
+ logger.LogError(e, "An error occured during migration");
+}
+
+await app.RunAsync();
