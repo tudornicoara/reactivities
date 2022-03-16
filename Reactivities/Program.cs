1d5b68c0058ca6567aac3c22208fb5bb098894d3
Tudor Nicoara <nicoaratudor@gmail.com>
2022-03-16 15:42:03 +0200

    Fixed password regex

--- Reactivities/Program.cs
+++ Reactivities/Program.cs
@@ -39,7 +39,7 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
+    // app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.Use(async (context, next) =>
     {
